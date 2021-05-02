11e95898a9ad1830ee38fcc42b009fa4a0cc1eac
rachelpeles <197303r@gmail.com>
2021-05-02 17:40:24 +0300

    lastCommit

---
+++ Plant.ts
@@ -0,0 +1,22 @@
+// class  Plant{
+//     name: string;
+//     plantNumber: number;
+//     season:Season
+    
+//     constructor(_Name: string,_plantNumber: number,_season:Season) {
+//       this.name = _Name;
+//       this.plantNumber=_plantNumber;
+//       this.season=_season;
+//     }
+//     life_span(monse: number = 0) {
+//       console.log(`${this.name} live ${monse} years.`);
+//     }
+//   }
+
+
+//   enum Season {
+//    Fall,
+//    Winter,
+//    Spring,
+//    Summer
+//   }
